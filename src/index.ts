187f42aab6b47fe090dde66ffb05b583591cb39c
Daniel <danielcvt@gmail.com>
2024-03-15 19:30:02 -0300

    1

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,23 @@
 import { Elysia } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia().get("/", () => "VogelCode-API")
+
+.get('/leads', async ()=>{  
+  const gsResponse = await fetch(
+    "https://script.google.com/macros/s/AKfycbwIAj1HWYmqEeF7I_A3WfJGoshnPzSbQLDYir00RhgoWs1QsRj5nLAsEUIAGYuD7DfopQ/exec",
+  );
+  let leads = await gsResponse.json();
+
+  leads = leads.slice(1).sort((a: string[], b: string[]) => {
+    return new Date(b[4]).getTime() - new Date(a[4]).getTime();
+  });
+  // leads = [new Date().toISOString()].concat(leads.slice(1));
+  return Response.json(leads);
+
+
+  return leads.json()
+})
+.listen(3000)
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
